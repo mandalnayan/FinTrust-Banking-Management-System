c84048c760abda49e95588ec7c6f9f05a4b30fae
Nayan Mandal <kumarnayan600@gmail.com>
2025-12-29 23:27:12 +0530

    merged admin account

--- src/main/java/com/fintrust/db/DBConnection.java
+++ src/main/java/com/fintrust/db/DBConnection.java
@@ -8,19 +8,15 @@ import java.sql.Statement;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.zkoss.zhtml.Main;
-
-import org.zkoss.zk.ui.util.Clients;
 
 import com.fintrust.util.NotificationUtil;
-
 
 public class DBConnection {
 
     // Database configuration constants
     private static final String URL = "jdbc:mysql://localhost:3306/fintrust_bank";
     private static final String USER = "root";
-    private static final String PASSWORD = "root123";
+    private static final String PASSWORD = "Nayan@2002";
     private static Connection connection = null;
 
     // Private constructor to prevent external instantiation
